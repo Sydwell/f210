80d787950186b51f4990696ca13ae40db0ff46a7
glorykas <glorykasongo07@gmail.com>
2020-11-09 10:50:31 +0200

    these files have been modified

--- src/app/prompt/prompt.component.ts
+++ src/app/prompt/prompt.component.ts
@@ -5,7 +5,10 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   templateUrl: './prompt.component.html',
   styleUrls: ['./prompt.component.css']
 })
+
+
 export class PromptComponent implements OnInit {
+  alert = false;
   @Input() userSupplied: string; // Becomes an [] attribute in the selector
   @Input() title: string;
   @Output() promptEvent = new EventEmitter<string>(); // Becomes an () attribute in the selector
@@ -19,5 +22,9 @@ export class PromptComponent implements OnInit {
   submitValue(value2View) {
    // this.value2Parent = value2View;
     this.promptEvent.emit(value2View);
+    this.alert = true;
+  }
+  closeAlert() {
+    this.alert = false;
   }
 }
